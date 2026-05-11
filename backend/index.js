cacd85205facfebd9d427ad307eb6348e5f1d7d3
gogopowercoding <furaihanalharits@gmail.com>
2026-05-11 18:06:29 +0700

    fix port5

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ const { createNoteTable } = require('./schemas/note');
     res.send('API Notes Backend berjalan');
   });
 
-  const PORT = process.env.PORT || 8080;
+  const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
